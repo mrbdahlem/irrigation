4119ef31f6e1190f23422bf7aad4dbbba174c7da
mrbdahlem <bdahlem@blueChrome>
2022-09-23 14:02:24 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const app = express();
+const fetch = require("node-fetch");
+const ical = require('ical-generator');
+const hostname = require('os').hostname();
+
+// https://expressjs.com/en/starter/basic-routing.html
+app.get("/:acct.ics", (request, response) => {
+    const acct = process.env.accountnum;
+
+    if (request.params.acct != acct) {
+        throw new Error("INVALID ACCOUNT " + request.params.acct);
+    }
+
+    const url = "https://water.gateway.srpnet.com/schedule/account/" + acct;
+    console.log(new Date() + " Requesting data for account number " + acct + " from ", url);
+
+    fetch(url)
+        .then(fetchResponse => {
+            if (!fetchResponse.ok) {
+                throw new Error(
+                    "Error fetching account data: " +
+                    fetchResponse.status +
+                    " " +
+                    fetchResponse.statusText
+                );
+            }
+            return fetchResponse.json();
+        })
+        .then(data => {
+            const cal = ical({
+                domain: hostname,
+                //timezone: process.env.timezone,
+                name: 'Irrigation',
+                url: request.url
+                });
+
+            const event = cal.createEvent({
+                id: data.id,
+                summary: "Irrigation: " + data.orderStatus,
+                description: data.irrigationNotice + '\nUpdated: ' + new Date(),
+                location: data.displayFirstAccountScheduleDetail.address,
+                start: data.onDateTime + process.env.tzoffset,
+                end: data.offDateTime + process.env.tzoffset,
+                //timezone: data.timezone,
+                alarms: [
+                    {
+                        type: 'display',
+                        trigger: 300
+                    },
+                    {
+                        type: 'audio',
+                        trigger: 1
+                    }
+                ]
+            });
+
+            event.createAlarm({
+                type: 'audio',
+                trigger: event.end()
+            })
+
+            response.setHeader('Cache-Control', 'no-cache');
+            response.type('text/calendar');
+            response.send(cal.toString());
+        });
+});
+
+app.get("/", (request, response) => {
+    response.send("Hi.");
+});
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, () => {
+    console.log("Irrigation Calendar is listening on port " + listener.address().port);
+});
